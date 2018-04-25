ebf1491ca8ec045042974a0d00ac4c44a44c9143
Gavin <f.c.hsu0723@gmail.com>
2018-04-25 11:56:39 +0800

    edit all.js

--- js/all.js
+++ js/all.js
@@ -10,7 +10,7 @@ updated();
 // 遠端讀取資料
 function updated(e){
 	const xhr = new XMLHttpRequest();
-	xhr.open('get', 'https://github.com/fchsu/Shopping-cart/blob/master/JSON/data.JSON', true);
+	xhr.open('get', '../JSON/data.JSON', true);
 	xhr.send(null);
 	console.log(xhr);
 }

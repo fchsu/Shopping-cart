7dc0e4dc9f74c34fdc28e1f4b0cef358fbc6145a
Gavin <f.c.hsu0723@gmail.com>
2018-05-12 07:28:40 +0800

    修正移動資料造成的變數名稱改變

--- js/all.js
+++ js/all.js
@@ -1,279 +1,4 @@
 // 全域變數設定
-// const productdata = {
-// 	"phone": 
-// 		[
-// 			{
-// 				"No.": "1",
-// 				"img": "img/phone/HTCUUltra64G574GB64GB.jpg",
-// 				"產品名稱": "HTC U Ultra 64G 5.7吋 雙螢幕雙卡智慧機(4GB  64GB)",
-// 				"價格": "$6,250",
-// 				"規格": 
-// 					[
-// 						"5.7 吋大小雙螢幕",
-// 						"S821四核處理器",
-// 						"主相機1,200 萬畫素",
-// 						"前鏡頭1,600 萬畫素",
-// 						"4G+3G雙卡雙待",
-// 						"4G RAM/64G ROM"
-// 					]
-// 			}, 
-// 			{
-// 				"No.": "2",
-// 				"img": "img/phone/SAMSUNGGalaxyS84G64G5.8吋防水旗艦機.jpg",
-// 				"產品名稱": "SAMSUNG Galaxy S8 (4G64G) 5.8吋 防水旗艦機",
-// 				"價格": "$13,699",
-// 				"規格": 
-// 					[
-// 						"(4G/64G) 5.8吋 近乎無邊的曲面設計",
-// 						"1200萬畫素後置鏡頭與800萬畫素前置相機",
-// 						"智慧快速自動對焦",
-// 						"虹膜辨識，智慧保護",
-// 						"IP68防水防塵"
-// 					]
-// 			},
-// 			{
-// 				"No.": "3",
-// 				"img": "img/phone/紅米Note4X5.5吋多彩金屬八核心智慧型手機小米.jpg",
-// 				"產品名稱": "紅米Note 4X 5.5吋 多彩金屬 八核心 智慧型手機 小米",
-// 				"價格": "$4,700",
-// 				"規格": 
-// 					[
-// 						"5.5 吋多彩金屬機身",
-// 						"4100mAh 超長續航",
-// 						"Snapdragon™ 8 核心處理器"
-// 					]
-// 			},
-// 			{
-// 				"No.": "4",
-// 				"img": "img/phone/AppleiPhone864GB智慧手機.jpg",
-// 				"產品名稱": "Apple iPhone 8 64GB 智慧手機",
-// 				"價格": "$20,000",
-// 				"規格": 
-// 					[
-// 						"4.7吋Retina HD顯示器",
-// 						"全玻璃機身/鋁金屬邊框",
-// 						"A11 Bionic節能4核心",
-// 						"12MP相機/光學影像穩定",
-// 						"IP67防潑抗水與防塵",
-// 						"無線充電/擴增實境"
-// 					]
-// 			},
-// 			{
-// 				"No.": "5",
-// 				"img": "img/phone/HTCU116G128G版5.5吋防水雙卡機.jpg",
-// 				"產品名稱": "HTC U11 6G128G版 5.5吋防水雙卡機",
-// 				"價格": "$10,490",
-// 				"規格": 
-// 					[
-// 						"作業系統與版本 Android 7.1",
-// 						"處理器品牌 Qualcomm",
-// 						"處理器型號 Snapdragon 835",
-// 						"主螢幕尺寸 5.5 inch",
-// 						"主相機畫素 1200 萬畫素",
-// 						"RAM記憶體 4 GB",
-// 						"ROM儲存空間 64 GB",
-// 						"電池容量 3000 mAh"
-// 					]
-// 			},
-// 			{
-// 				"No.": "6",
-// 				"img": "img/phone/SamsungGalaxyJ7Pro.jpg",
-// 				"產品名稱": "Samsung Galaxy J7 Pro",
-// 				"價格": "$5,500",
-// 				"規格": 
-// 					[
-// 						"★全金屬機身，相機鏡頭平整，背蓋零外凸",
-// 						"★前後鏡頭 1300 萬畫素，主鏡頭 F1.7大光圈★八核心處理器 + 3GB RAM",
-// 						"★安全資料夾，資安防護",
-// 						"★指紋辨識",
-// 						"★4G + 3G 雙卡雙待，獨立3卡槽"
-// 					]
-// 			},
-// 			{
-// 				"No.": "7",
-// 				"img": "img/phone/SamsungGalaxyNote86G64G6.3吋雙卡智慧機皇.jpg",
-// 				"產品名稱": "Samsung Galaxy Note 8 (6G64G) 6.3 吋雙卡智慧機皇",
-// 				"價格": "$19,900",
-// 				"規格": 
-// 					[
-// 						"登錄送原廠延長保固一年",
-// 						"6.3吋雙曲面無邊際螢幕",
-// 						"Spen超神進化 寫、畫、譯無所不能",
-// 						"防震雙鏡頭 柔美景深 夜拍清晰",
-// 						"提前到貨提前出貨"
-// 					]
-// 			},
-// 			{
-// 				"No.": "8",
-// 				"img": "img/phone/HTCU11Plus4G64G&6G128G.jpg",
-// 				"產品名稱": "HTC U11 Plus 4G/64G & 6G/128G",
-// 				"價格": "$12,900",
-// 				"規格": 
-// 					[
-// 						"★6 吋 Quad HD 顯示 18:9寬螢幕",
-// 						"★Edge Sense 側框感應",
-// 						"★水漾玻璃表面和三軸對稱曲線",
-// 						"★DCI-P3 廣色域，色彩飽合度更高",
-// 						"★1200 萬畫素 UltraPixel™ 3 主相機，1.4μm 畫素"
-// 					]
-// 			},
-// 			{
-// 				"No.": "9",
-// 				"img": "img/phone/OPPOA57CPH17013GB23GB.jpg",
-// 				"產品名稱": "OPPO A57(CPH1701) 3GB23GB",
-// 				"價格": "$4,690",
-// 				"規格": 
-// 					[
-// 						"★1600萬畫素美顏自拍",
-// 						"★Give me Five手勢自拍，背景虛化",
-// 						"★1300萬畫素主鏡頭",
-// 						"★SONY堆疊式CMOS 感光元件，PDAF相位對焦技術",
-// 						"★極速指紋辨識，快速、準確又安全",
-// 						"★八核心64位元處理器，高性能、低功耗",
-// 						"★3GB RAM+32GB ROM，極速運行、高效性能",
-// 						"★2900mAh大容量電池"
-// 					]
-// 			}
-// 		],
-// 	"notebook":
-// 		[
-// 			{
-// 				"No.": "1",
-// 				"img": "img/notebook/ASUS華碩15.6吋電競筆電i77700HQ8G1TBGTX10504G.jpg",
-// 				"產品名稱": "ASUS華碩 15.6吋電競筆電 i7-7700HQ8G1TBGTX1050-4G ",
-// 				"價格": "$24,888",
-// 				"規格": 
-// 					[
-// 						"Intel i7-7700HQ處理器",
-// 						"1TB硬碟(7200轉)",
-// 						"NVIDIA GTX1050 4G獨顯"
-// 					]
-// 			},
-// 			{
-// 				"No.": "2",
-// 				"img": "img/notebook/AppleMacBookAir13.3吋1.68G128GFlashMMGF2TAA.jpg",
-// 				"產品名稱": "【Apple】 MacBook Air 13.3吋 1.68G128G Flash(MMGF2TAA)",
-// 				"價格": "$40,390",
-// 				"規格": 
-// 					[
-// 						"◎★處理器 1.6GHz 雙核心 Intel Core i5",
-// 						"◎★ 8GB 記憶體 / 128GB 快閃儲存",
-// 						"◎★作業系統 OS X Yosemite"
-// 					]
-// 			},
-// 			{
-// 				"No.": "3",
-// 				"img": "img/notebook/LenovoYogaBookZA150073TW10.1吋二合一平板筆電.jpg",
-// 				"產品名稱": "Lenovo Yoga Book ZA150073TW 10.1吋 二合一平板筆電",
-// 				"價格": "$12,880",
-// 				"規格": 
-// 					[
-// 						"極致纖薄。時尚設計",
-// 						"■處理器：Intel Atom x5-Z8550四核心",
-// 						"■記憶體：4 GB",
-// 						"■硬碟：64 GB",
-// 						"■螢幕：10.1 吋 FHD IPS(1920 x 1200)",
-// 						"■作業系統：Windows 10 家用版",
-// 						"■二合一平板電腦"
-// 					]
-// 			},
-// 			{
-// 				"No.": "4",
-// 				"img": "img/notebook/MicrosoftSurfacePro48Gi5256G.jpg",
-// 				"產品名稱": "Microsoft Surface Pro 4 8G/i5-256G",
-// 				"價格": "$28,400",
-// 				"規格": 
-// 					[
-// 						"處理器：Intel® 第6代Core™ i5",
-// 						"記憶體：8 GB RAM",
-// 						"儲存容量：256GB SSD",
-// 						"重量：786G",
-// 						"作業系統：Windows 10 Pro",
-// 						"圖形晶片：Intel® HD Graphics",
-// 						"螢幕：12.3吋 PixelSense顯示器",
-// 						"解析度：2736x1824 (267PPI)",
-// 						"外觀比例：3：2",
-// 						"觸控：10點多點觸控",
-// 						"光碟機：無",
-// 						"連接阜：全尺寸USB 3.0/Micro SD讀卡機/耳機插孔/Mini DisplayPort/保護蓋連接阜/SurfaceConnect",
-// 						"無線網路：802.11ac WiFi無線網路 相容802.11a/b/g/n Bluetooth4.0無線技術",
-// 						"感應器：週遭環境光感應器/加速計/陀螺儀",
-// 						"相機/視訊/音訊：5百萬像素前方HD相機 8百萬像素後方自動對焦相機，配備1080p HD視訊錄製功能 立體聲麥克風 具備Dolbu音效的立體聲喇叭",
-// 						"電池續航力：播放視訊可達9小時",
-// 						"安全性：支援企業安全性的TPM晶片"
-// 					]
-// 			},
-// 			{
-// 				"No.": "5",
-// 				"img": "img/notebook/ASUS華碩10吋觸控平板筆電Z83504G64GT102HA.jpg",
-// 				"產品名稱": "ASUS華碩 10吋觸控平板筆電 Z83504G64G (T102HA)",
-// 				"價格": "$11,000",
-// 				"規格": 
-// 					[
-// 						"最新四核Intel Trail",
-// 						"輕薄變形二合一，790g",
-// 						"USB 3.1 C 型連接埠"
-// 					]
-// 			},
-// 			{
-// 				"No.": "6",
-// 				"img": "img/notebook/ASUS華碩14吋筆電i57200U4G256GSSD940MX2GUX410UQ.jpg",
-// 				"產品名稱": "ASUS華碩 14吋筆電 i5-7200U4G256G SSD940MX-2G (UX410UQ)",
-// 				"價格": "$24,999",
-// 				"規格": 
-// 					[
-// 						"Intel i5-7200U處理器",
-// 						"4G記憶體 / 256G SSD",
-// 						"NVIDIA 940MX 2G獨顯"
-// 					]
-// 			},
-// 			{
-// 				"No.": "7",
-// 				"img": "img/notebook/LenovoIdeaPadY52015.6吋電競筆電.jpg",
-// 				"產品名稱": "Lenovo IdeaPad Y520 15.6吋電競筆電",
-// 				"價格": "$23,900",
-// 				"規格": 
-// 					[
-// 						"●網路功能：100/1000M",
-// 						"●無線網路：802.11 AC",
-// 						"●藍芽：支援 BT 4.1",
-// 						"●視訊：720P HD",
-// 						"●音效：2.0 Harman 雙立體喇叭",
-// 						"●連接埠：1xUSB 2.0、2xUSB 3.0、1xType C",
-// 						"●讀卡機：四合一讀卡機 (SD/SDHC/SDXC/MMC)",
-// 						"●電池容量：3 Cell (45Wh) / 4hr",
-// 						"●尺寸：約 2.4 Kg",
-// 						"●重量：約 380 x 265 x 25.8 mm (W x D x H)"
-// 					]
-// 			},
-// 			{
-// 				"No.": "8",
-// 				"img": "img/notebook/ASUS華碩筆記型電腦X556UR15.6吋.jpg",
-// 				"產品名稱": "ASUS華碩 筆記型電腦 X556UR 15.6吋",
-// 				"價格": "$16,800",
-// 				"規格": 
-// 					[
-// 						"配備USB 3.1 type-C 正反皆可插",
-// 						"全尺寸孤島式鍵盤(含獨立數字鍵)",
-// 						"ASUS Splendid 色溫校正功能",
-// 						"ASUS Eye Care抗藍光護眼技術",
-// 						"SonicMaster聲籟技術"
-// 					]
-// 			},
-// 			{
-// 				"No.": "9",
-// 				"img": "img/notebook/ASUSE203NA0051EN335011.6吋小筆電.jpg",
-// 				"產品名稱": "ASUS E203NA-0051EN3350 11.6吋小筆電",
-// 				"價格": "$6,790",
-// 				"規格": 
-// 					[
-// 						"Intel 雙核 Celeron® N3350",
-// 						"4G記憶體 / EMMC32G",
-// 						"輕薄980g"
-// 					]
-// 			}
-// 		]
-// }
 
 
 // 動作與監聽
@@ -284,12 +9,13 @@ window.addEventListener('popstate', historyPage, false);
 // 函式設定
 	// 更新畫面顯示內容
 function updated(e){
+	// let siteUrl = location.search;
 	const xhr = new XMLHttpRequest();
 	xhr.open('GET', 'json/data.JSON', true);
 	xhr.onload = (e) => {
 		if (xhr.readyState === 4){
 			// if (xhrReg.status === 200){
-				const data = xhr.responseText;
+				const data = JSON.parse(xhr.responseText);
 				let vmContent = new Vue({
 					el: '#content',
 					data: {
@@ -313,7 +39,7 @@ function updated(e){
 					methods: {
 						// 點選 phone 分類更改商品顯示內容
 						clickPhone: function(e){
-							this.product.splice(0, 2, productdata.phone);
+							this.product.splice(0, 2, data.phone);
 							this.classStatus.phoneOn = true;
 							this.classStatus.notebookOn = false;
 							this.judgment.listButtom = true;
@@ -325,7 +51,7 @@ function updated(e){
 						},
 						// 點選 notebook 分類更改商品顯示內容
 						clickNotebook: function(e){
-							this.product.splice(0, 2, productdata.notebook);
+							this.product.splice(0, 2, data.notebook);
 							this.classStatus.phoneOn = false;
 							this.classStatus.notebookOn = true;
 							this.judgment.listButtom = true;
@@ -339,11 +65,11 @@ function updated(e){
 						clickList: function(index){
 							let Len = this.shoppingCart.length;
 							switch (this.product[0]){
-								case productdata.phone:
-									this.product.splice(0, 2, productdata.phone[index]);
+								case data.phone:
+									this.product.splice(0, 2, data.phone[index]);
 									break;
-								case productdata.notebook:
-									this.product.splice(0, 2, productdata.notebook[index]);
+								case data.notebook:
+									this.product.splice(0, 2, data.notebook[index]);
 									break;
 							}
 							this.judgment.listButtom = false;
@@ -377,8 +103,7 @@ function updated(e){
 							}, 'shoppingCart', '?page=shoppingCart');
 						},
 						// 點擊減少(-)按鈕，數量減1
-						clickReduce: function(e){
-							const index = e.currentTarget.nextElementSibling.dataset.index;
+						clickReduce: function(index, e){
 							let inputNumber = e.currentTarget.nextElementSibling;
 							if (this.shoppingNumber[index] > 1){
 								this.shoppingNumber[index]--;
@@ -387,23 +112,20 @@ function updated(e){
 							}
 						},
 						// 點擊增加(+)按鈕，數量加1
-						clickIncrease: function(e){
-							const index = e.currentTarget.previousElementSibling.dataset.index;
+						clickIncrease: function(index, e){
 							let inputNumber = e.currentTarget.previousElementSibling;
 							if (this.shoppingNumber[index] < 100){
 								this.shoppingNumber[index]++;
 								inputNumber.value++;
 								localStorage.setItem("shoppingNumberRecord", JSON.stringify(this.shoppingNumber));
 							}
-							this.count(index);
+							// this.count(index);
 						},
-
-						
 						// 依數量計算採購小計
-						count: function(number){
-							const price = this.shoppingCart[number]['價格'].slice(1);
+						count: function(index){
+							const price = this.shoppingCart[index]['價格'].slice(1);
 							const priceNumber = parseInt(price.slice(-11, -8) + price.slice(-7, -4) + price.slice(-3));
-							const cartNumber = this.shoppingNumber[number];
+							const cartNumber = this.shoppingNumber[index];
 							const SubtotalPrice = priceNumber * cartNumber;
 							return SubtotalPrice;
 						}
@@ -435,141 +157,3 @@ function updated(e){
 function historyPage(e){
 	console.log(e.state.sitePosition);
 }
-	// 讀取資料
-// let vmContent = new Vue({
-// 	el: '#content',
-// 	data: {
-// 		product: [productdata.phone, productdata.notebook],
-// 		shoppingCart: JSON.parse(localStorage.getItem("shoppingCartRecord")) || [],
-// 		shoppingNumber: JSON.parse(localStorage.getItem("shoppingNumberRecord")) || [],
-// 		classStatus: {
-// 			hoverOn: true,
-// 			phoneOn: true,
-// 			notebookOn: false
-// 		},
-// 		judgment: {
-// 			productOn: true,
-// 			shoppingCartOn: false,
-// 			listButtom: true,
-// 			detailButtom: false,
-// 			inputCartButtom: true,
-// 			alreadyShoppingButtom: false
-// 		}
-// 	},
-// 	methods: {
-// 		// 點選 phone 分類更改商品顯示內容
-// 		clickPhone: function(e){
-// 			this.product.splice(0, 2, productdata.phone);
-// 			this.classStatus.phoneOn = true;
-// 			this.classStatus.notebookOn = false;
-// 			this.judgment.listButtom = true;
-// 			this.judgment.detailButtom = false;
-// 			history.pushState({
-// 				sitePosition: 'phone',
-// 				url: '/phone'
-// 			}, 'phone', '?page=phone');
-// 		},
-// 		// 點選 notebook 分類更改商品顯示內容
-// 		clickNotebook: function(e){
-// 			this.product.splice(0, 2, productdata.notebook);
-// 			this.classStatus.phoneOn = false;
-// 			this.classStatus.notebookOn = true;
-// 			this.judgment.listButtom = true;
-// 			this.judgment.detailButtom = false;
-// 			history.pushState({
-// 				sitePosition: 'notebook',
-// 				url: '/notebook'
-// 			}, 'notebook', '?page=notebook');
-// 		},
-// 		// 點選單一商品顯示詳細內容
-// 		clickList: function(index){
-// 			let Len = this.shoppingCart.length;
-// 			switch (this.product[0]){
-// 				case productdata.phone:
-// 					this.product.splice(0, 2, productdata.phone[index]);
-// 					break;
-// 				case productdata.notebook:
-// 					this.product.splice(0, 2, productdata.notebook[index]);
-// 					break;
-// 			}
-// 			this.judgment.listButtom = false;
-// 			this.judgment.detailButtom = true;
-// 			this.judgment.inputCartButtom = true;
-// 			this.judgment.alreadyShoppingButtom = false;
-// 			for (let i = 0; i < Len; i++){
-// 				if (this.shoppingCart[i]['No.'] === this.product[0]['No.'] && this.shoppingCart[i]['產品名稱'] === this.product[0]['產品名稱']){
-// 				this.judgment.inputCartButtom = false;
-// 				this.judgment.alreadyShoppingButtom = true;
-// 				}
-// 			}
-// 		},
-// 		// 點擊加入購物車按鈕
-// 		clickInputCart: function(e){
-// 			this.judgment.inputCartButtom = false;
-// 			this.judgment.alreadyShoppingButtom = true;
-// 			vmCart.InputCart();
-// 			this.shoppingCart.push(this.product[0]);
-// 			this.shoppingNumber.push(1);
-// 			localStorage.setItem("shoppingCartRecord", JSON.stringify(this.shoppingCart));
-// 			localStorage.setItem("shoppingNumberRecord", JSON.stringify(this.shoppingNumber));
-// 		},
-// 		// 更換顯示內容 產品列表 → 購物列表
-// 		changePage: function(e){
-// 			this.judgment.productOn = false;
-// 			this.judgment.shoppingCartOn = true;
-// 			history.pushState({
-// 				sitePosition: 'shoppingCart',
-// 				url: '/shoppingCart'
-// 			}, 'shoppingCart', '?page=shoppingCart');
-// 		},
-// 		// 點擊減少(-)按鈕，數量減1
-// 		clickReduce: function(e){
-// 			const index = e.currentTarget.nextElementSibling.dataset.index;
-// 			let inputNumber = e.currentTarget.nextElementSibling;
-// 			if (this.shoppingNumber[index] > 1){
-// 				this.shoppingNumber[index]--;
-// 				inputNumber.value--;
-// 				localStorage.setItem("shoppingNumberRecord", JSON.stringify(this.shoppingNumber));
-// 			}
-// 		},
-// 		// 點擊增加(+)按鈕，數量加1
-// 		clickIncrease: function(e){
-// 			const index = e.currentTarget.previousElementSibling.dataset.index;
-// 			let inputNumber = e.currentTarget.previousElementSibling;
-// 			if (this.shoppingNumber[index] < 100){
-// 				this.shoppingNumber[index]++;
-// 				inputNumber.value++;
-// 				localStorage.setItem("shoppingNumberRecord", JSON.stringify(this.shoppingNumber));
-// 			}
-// 			this.count(index);
-// 		},
-
-		
-// 		// 依數量計算採購小計
-// 		count: function(number){
-// 			const price = this.shoppingCart[number]['價格'].slice(1);
-// 			const priceNumber = parseInt(price.slice(-11, -8) + price.slice(-7, -4) + price.slice(-3));
-// 			const cartNumber = this.shoppingNumber[number];
-// 			const SubtotalPrice = priceNumber * cartNumber;
-// 			return SubtotalPrice;
-// 		}
-// 	}
-// });
-	// 購物車記數
-// let vmCart = new Vue({
-// 	el: '#header',
-// 	data: {
-// 		number: vmContent.shoppingCart.length,
-// 		classStatus: {
-// 			hoverOn: true,
-// 		},
-// 	},
-// 	methods: {
-// 		InputCart: function(e){
-// 			vmCart.number++;
-// 		},
-// 		clickShoppingCart: function(e){
-// 			vmContent.changePage();
-// 		}
-// 	}
-// });
